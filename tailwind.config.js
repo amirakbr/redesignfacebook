1d2f478bb40388d1b285652ff383b970269e9a38
Amir <amir.akbrzadh3@gmail.com>
2022-11-12 00:50:10 +0330

    add some variables for tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -16,6 +16,11 @@ module.exports = {
         'emojiPostSection': '2.25rem',
         'emojiMarginPostSection': '.4rem',
         'profilePicture' : '3rem' ,
+        'emojiPost' : '1.6rem' ,
+        'emojiPostSpace' : '0.3rem' ,
+        'largeEmojiPost' : '1.5rem' ,
+        'largeEmojiPostSpace' : '.2rem' ,
+        
       },
       screens: {
         //
